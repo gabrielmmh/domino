0d5b7ec13f1ad41d3eebec57ec3db7fc34e34db0
gabrielmmh <92440743+gabrielmmh@users.noreply.github.com>
2021-11-03 20:34:12 -0300

    Update Domino_versao_1.py

--- Domino_versao_1.py
+++ Domino_versao_1.py
@@ -3,7 +3,7 @@ if pergunta == 'nao':
     print('Então pq tentou?')
     
 if pergunta == 'sim':
-    num_players=int(input('Pra quantas pessoas deseja perder?  Numero máximo (4) :  '))
+    num_players=int(input('Contando você, quantas pessoas irão jogar? (2 a 4 jogadores) '))
     players=[]
     while num_players>0:
         players.append(num_players)
