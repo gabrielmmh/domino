84129bef4a8e388d402050d2df4b6557abd37392
Lucas Tachdjian <lucasct1@al.insper.edu.br>
2021-11-03 15:40:42 -0300

    Segundo Commit-Inicio do jogo e def de players-LT

---
+++ Domino_versao_1.py
@@ -0,0 +1,10 @@
+pergunta=str(input('Deseja jogar domino dos Deuses?  Responda com "sim" ou "nao"  '))
+if pergunta == 'nao':
+    print('Então pq tentou?')
+    
+if pergunta == 'sim':
+    num_players=int(input('Pra quantas pessoas deseja perder?  Numero máximo (4) :  '))
+    players=[]
+    while num_players>0:
+        players.append(num_players)
+        num_players-=1
